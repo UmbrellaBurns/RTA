39869da8eed12f139df4f22180a09dde478b2f60
Ereon <umbrellaburns@gmail.com>
2017-03-10 18:59:34 +0300

    Add splash screen

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from PyQt5.QtWidgets import QApplication
 from ui.main_window import MainWindow
+from ui.splash_screen import SplashScreen
 import sys
+
 
 if __name__ == '__main__':
     if __name__ == "__main__":
         app = QApplication(sys.argv)
+
+        s = SplashScreen("common/splash.gif")
+        s.show()
+
         w = MainWindow()
         w.resize(700, 500)
+
+        s.processing(7, app)
+        s.finish(w)
+
         w.show()
         sys.exit(app.exec())
 
-        # TODO: MophologicalAnalysis + pymorphy2 - Singleton, Main Application - Facade.
         # TODO: SyntaxAnalysis: methods - get_syntax_xml_tree, get_raw_xml.
-        # TODO: Proposal: make Doc classes - Sentences, Nodes,..
         # TODO: ComplexAnalysis result - xml-doc, which contains document markup, including: text, sentences,
         # TODO: syntax trees with nodes and their relations, semantic graph, text category and more
---
+++ ui/splash_screen.py
@@ -0,0 +1,47 @@
+import sys, time
+from PyQt5.QtCore import Qt, QTimer
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QSplashScreen, QApplication, QWidget
+
+
+class SplashScreen(QSplashScreen):
+    """
+    Splash screen class that appears during component initialization
+    """
+    def __init__(self, gif, parent=None):
+
+        self.__movie = QMovie(gif)
+
+        self.__movie.jumpToFrame(0)
+        pixmap = QPixmap(self.__movie.frameRect().size())
+
+        QSplashScreen.__init__(self, pixmap)
+
+        self.__movie.frameChanged.connect(self.repaint)
+
+    def processing(self, duration, app):
+        """
+        Splash screen exec.
+        :param duration: duration of splash screen processing
+        :param app: application object, required to process events
+        """
+        start = time.time()
+
+        while self.__movie.state() == QMovie.Running and time.time() < start + duration:
+            app.processEvents()
+
+    def showEvent(self, event):
+        self.__movie.start()
+
+    def hideEvent(self, event):
+        self.__movie.stop()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        pixmap = self.__movie.currentPixmap()
+        self.setMask(pixmap.mask())
+        painter.drawPixmap(0, 0, pixmap)
+
+    def sizeHint(self):
+        return self.__movie.scaledSize()
